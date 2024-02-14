33394793a84ecc419f3a50ce27fa194021f252f0
Leandro Avila <Lavilal@uteq.edu.ec>
2024-02-14 10:20:23 -0500

    nuevoReconocimiento

--- app/src/main/java/com/example/visionapi/MainActivity.java
+++ app/src/main/java/com/example/visionapi/MainActivity.java
@@ -18,6 +18,8 @@ import android.Manifest;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.visionapi.ml.Modeloflowers;
+import com.example.visionapi.ml.Modelorostro;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -36,7 +38,13 @@ import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -96,6 +104,52 @@ public class MainActivity extends AppCompatActivity
                 .addOnSuccessListener(this)
                 .addOnFailureListener(this);
     }
+
+    public void PersonalizedModel(View v){
+        try {
+            String[] etiquetas = {"Fernando", "Solange", "Domenica", "Leandro", "Holger"};
+            Modelorostro model = Modelorostro.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
+            Modelorostro.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            txtResults.setText(obtenerEtiquetayProbabilidad(etiquetas, outputFeature0.getFloatArray()));
+            model.close();
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
+        }
+    }
+
+    public ByteBuffer convertirImagenATensorBuffer(Bitmap mSelectedImage){
+        Bitmap imagen = Bitmap.createScaledBitmap(mSelectedImage, 224, 224, true);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        int[] intValues = new int[224 * 224];
+        imagen.getPixels(intValues, 0, imagen.getWidth(), 0, 0, imagen.getWidth(), imagen.getHeight());
+        int pixel = 0;
+        for(int i = 0; i < imagen.getHeight(); i ++){
+            for(int j = 0; j < imagen.getWidth(); j++){
+                int val = intValues[pixel++]; // RGB
+                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+            }
+        }
+        return byteBuffer;
+    }
+    public String obtenerEtiquetayProbabilidad(String[] etiquetas, float[] probabilidades){
+        float valorMayor=Float.MIN_VALUE;
+        int pos=-1;
+        for (int i = 0; i < probabilidades.length; i++) {
+            if (probabilidades[i] > valorMayor) {
+                valorMayor = probabilidades[i];
+                pos = i;
+            }
+        }
+        return "Predicción: " + etiquetas[pos] + ", Probabilidad: " +
+                (new DecimalFormat("0.00").format(probabilidades[pos] * 100)) + "%";
+    }
+
 
     @Override
     public void onFailure(@NonNull Exception e) {
@@ -174,3 +228,4 @@ public class MainActivity extends AppCompatActivity
     }
 
 }
+
